2ed4db83010ef1dd3b90f9fc43ec92a6ed952df5
adejumo samuel <adejumosamuel108@gmail.com>
2024-11-24 20:01:14 +0100

    First commit

---
+++ .gradle/8.4/dependencies-accessors/f6dfdce2ca2a4ba8b44e4c3857260a707cc54647/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,409 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final EspressoLibraryAccessors laccForEspressoLibraryAccessors = new EspressoLibraryAccessors(owner);
+    private final ExtLibraryAccessors laccForExtLibraryAccessors = new ExtLibraryAccessors(owner);
+    private final FirebaseLibraryAccessors laccForFirebaseLibraryAccessors = new FirebaseLibraryAccessors(owner);
+    private final GoogleLibraryAccessors laccForGoogleLibraryAccessors = new GoogleLibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+        /**
+         * Creates a dependency provider for activity (androidx.activity:activity)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getActivity() {
+            return create("activity");
+    }
+
+        /**
+         * Creates a dependency provider for appcompat (androidx.appcompat:appcompat)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getAppcompat() {
+            return create("appcompat");
+    }
+
+        /**
+         * Creates a dependency provider for cardview (androidx.cardview:cardview)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getCardview() {
+            return create("cardview");
+    }
+
+        /**
+         * Creates a dependency provider for constraintlayout (androidx.constraintlayout:constraintlayout)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getConstraintlayout() {
+            return create("constraintlayout");
+    }
+
+        /**
+         * Creates a dependency provider for junit (junit:junit)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getJunit() {
+            return create("junit");
+    }
+
+        /**
+         * Creates a dependency provider for material (com.google.android.material:material)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getMaterial() {
+            return create("material");
+    }
+
+    /**
+     * Returns the group of libraries at espresso
+     */
+    public EspressoLibraryAccessors getEspresso() {
+        return laccForEspressoLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of libraries at ext
+     */
+    public ExtLibraryAccessors getExt() {
+        return laccForExtLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of libraries at firebase
+     */
+    public FirebaseLibraryAccessors getFirebase() {
+        return laccForFirebaseLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of libraries at google
+     */
+    public GoogleLibraryAccessors getGoogle() {
+        return laccForGoogleLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Returns the group of bundles at bundles
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class EspressoLibraryAccessors extends SubDependencyFactory {
+
+        public EspressoLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for core (androidx.test.espresso:espresso-core)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getCore() {
+                return create("espresso.core");
+        }
+
+    }
+
+    public static class ExtLibraryAccessors extends SubDependencyFactory {
+
+        public ExtLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for junit (androidx.test.ext:junit)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getJunit() {
+                return create("ext.junit");
+        }
+
+    }
+
+    public static class FirebaseLibraryAccessors extends SubDependencyFactory {
+
+        public FirebaseLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for analytics (com.google.firebase:firebase-analytics)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getAnalytics() {
+                return create("firebase.analytics");
+        }
+
+            /**
+             * Creates a dependency provider for auth (com.google.firebase:firebase-auth)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getAuth() {
+                return create("firebase.auth");
+        }
+
+            /**
+             * Creates a dependency provider for bom (com.google.firebase:firebase-bom)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getBom() {
+                return create("firebase.bom");
+        }
+
+            /**
+             * Creates a dependency provider for database (com.google.firebase:firebase-database)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getDatabase() {
+                return create("firebase.database");
+        }
+
+    }
+
+    public static class GoogleLibraryAccessors extends SubDependencyFactory {
+
+        public GoogleLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for services (com.google.gms:google-services)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getServices() {
+                return create("google.services");
+        }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        private final FirebaseVersionAccessors vaccForFirebaseVersionAccessors = new FirebaseVersionAccessors(providers, config);
+        private final PlayVersionAccessors vaccForPlayVersionAccessors = new PlayVersionAccessors(providers, config);
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: activity (1.8.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getActivity() { return getVersion("activity"); }
+
+            /**
+             * Returns the version associated to this alias: agp (8.3.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getAgp() { return getVersion("agp"); }
+
+            /**
+             * Returns the version associated to this alias: appcompat (1.7.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getAppcompat() { return getVersion("appcompat"); }
+
+            /**
+             * Returns the version associated to this alias: cardview (1.0.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getCardview() { return getVersion("cardview"); }
+
+            /**
+             * Returns the version associated to this alias: constraintlayout (2.1.4)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getConstraintlayout() { return getVersion("constraintlayout"); }
+
+            /**
+             * Returns the version associated to this alias: espressoCore (3.6.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getEspressoCore() { return getVersion("espressoCore"); }
+
+            /**
+             * Returns the version associated to this alias: googleServices (4.4.2)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getGoogleServices() { return getVersion("googleServices"); }
+
+            /**
+             * Returns the version associated to this alias: junit (4.13.2)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getJunit() { return getVersion("junit"); }
+
+            /**
+             * Returns the version associated to this alias: junitVersion (1.2.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getJunitVersion() { return getVersion("junitVersion"); }
+
+            /**
+             * Returns the version associated to this alias: material (1.12.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getMaterial() { return getVersion("material"); }
+
+        /**
+         * Returns the group of versions at versions.firebase
+         */
+        public FirebaseVersionAccessors getFirebase() {
+            return vaccForFirebaseVersionAccessors;
+        }
+
+        /**
+         * Returns the group of versions at versions.play
+         */
+        public PlayVersionAccessors getPlay() {
+            return vaccForPlayVersionAccessors;
+        }
+
+    }
+
+    public static class FirebaseVersionAccessors extends VersionFactory  implements VersionNotationSupplier {
+
+        public FirebaseVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Returns the version associated to this alias: firebase (17.2.2)
+         * If the version is a rich version and that its not expressible as a
+         * single version string, then an empty string is returned.
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> asProvider() { return getVersion("firebase"); }
+
+            /**
+             * Returns the version associated to this alias: firebase.auth (22.0.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getAuth() { return getVersion("firebase.auth"); }
+
+            /**
+             * Returns the version associated to this alias: firebase.bom (33.6.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getBom() { return getVersion("firebase.bom"); }
+
+            /**
+             * Returns the version associated to this alias: firebase.database (19.2.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getDatabase() { return getVersion("firebase.database"); }
+
+    }
+
+    public static class PlayVersionAccessors extends VersionFactory  {
+
+        private final PlayServicesVersionAccessors vaccForPlayServicesVersionAccessors = new PlayServicesVersionAccessors(providers, config);
+        public PlayVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Returns the group of versions at versions.play.services
+         */
+        public PlayServicesVersionAccessors getServices() {
+            return vaccForPlayServicesVersionAccessors;
+        }
+
+    }
+
+    public static class PlayServicesVersionAccessors extends VersionFactory  {
+
+        public PlayServicesVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: play.services.location (21.0.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getLocation() { return getVersion("play.services.location"); }
+
+            /**
+             * Returns the version associated to this alias: play.services.maps ( 18.1.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getMaps() { return getVersion("play.services.maps"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Creates a plugin provider for androidApplication to the plugin id 'com.android.application'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getAndroidApplication() { return createPlugin("androidApplication"); }
+
+    }
+
+}
